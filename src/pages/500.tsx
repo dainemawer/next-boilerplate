0050f1ac858b241e03ff9f20af6a7e288e528101
Daine Mawer <hello@dainemawer.com>
2022-11-15 17:53:44 +0200

    feat: added error pages

---
+++ src/pages/500.tsx
@@ -0,0 +1,17 @@
+/*
+ * 500 Error Page
+ *
+ * Server-rendering an error page for every visit adds
+ * complexity to responding to errors.
+ *
+ * @see https://nextjs.org/docs/advanced-features/custom-error-page
+ * @returns {JSX.Element}
+*/
+
+export default function FiveZeroZero(): JSX.Element {
+	return (
+		<div>
+			<h1>500 - Internal Server Error</h1>
+		</div>
+	)
+}
