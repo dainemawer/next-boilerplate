88eec6a832d8fba71074a85acb85f3572077f172
Daine Mawer <hello@dainemawer.com>
2022-11-15 17:53:57 +0200

    feat: added default home page

---
+++ src/pages/index.tsx
@@ -0,0 +1,15 @@
+/*
+ * Home
+ *
+ * Homepage template for site application.
+ *
+ * @returns {JSX.Element}
+*/
+
+export default function Home(): JSX.Element {
+	return (
+		<div>
+			<h1>Home</h1>
+		</div>
+	)
+}
