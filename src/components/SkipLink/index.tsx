d1bb60046bf8769a343b5f9a33ac7a8a1d8352f7
Daine Mawer <hello@dainemawer.com>
2022-11-16 16:16:33 +0200

    feat: add global skiplink

---
+++ src/components/SkipLink/index.tsx
@@ -0,0 +1,13 @@
+/*
+ * SkipLink
+ *
+ * Site Main Content Skip Link
+ *
+ * @returns {JSX.Element}
+*/
+
+export default function SkipLink(): JSX.Element {
+	return (
+		<a href="#main-content" className="sr-only">Skip to main content</a>
+	)
+}
