50a145d36beb052b297d8a8d4ebedc3eb2b8ead7
Daine Mawer <hello@dainemawer.com>
2022-11-16 16:58:15 +0200

    feat: updated analytics call to use script

--- src/lib/gtag/analytics.tsx
+++ src/lib/gtag/analytics.tsx
@@ -7,30 +7,37 @@
  * @returns {JSX.Element}
 */
 
-import Script from 'next/script'
 import { GA_TRACKING_ID } from '.'
 
 export const GoogleAnalytics = (): JSX.Element => {
 	return (
 		<>
-			<Script
-				strategy="afterInteractive"
+			<script
+				async
 				src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`}
 			/>
-			<Script
-				id="gtag-init"
-				strategy="afterInteractive"
+			<script
 				dangerouslySetInnerHTML={{
 					__html: `
-						window.dataLayer = window.dataLayer || [];
-						function gtag(){dataLayer.push(arguments);}
-						gtag('js', new Date());
-						gtag('config', '${GA_TRACKING_ID}', {
-							page_path: window.location.pathname,
-						});
-					`,
+							window.dataLayer = window.dataLayer || [];
+							function gtag(){dataLayer.push(arguments);}
+							gtag('js', new Date());
+							gtag('config', '${GA_TRACKING_ID}', {
+								page_path: window.location.pathname,
+							});
+						`,
 				}}
 			/>
 		</>
 	)
+}
+
+export const sendToAnalytics = (id, name, label, value) => {
+	window.gtag('event', name, {
+		event_category:
+			label === 'web-vital' ? 'Web Vitals' : 'Next.js custom metric',
+		value: Math.round(name === 'CLS' ? value * 1000 : value),
+		event_label: id,
+		non_interaction: true,
+	});
 }
